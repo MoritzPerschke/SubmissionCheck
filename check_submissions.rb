4f2c30676ae103804325f7749796e804be672eb3
Moritz Perschke <perschkemoritz@gmail.com>
2025-03-12 11:54:32 +0100

    handles and extracts submitted zip files, checks for unwanted extensions and filenames

--- check_submissions.rb
+++ check_submissions.rb
@@ -5,11 +5,12 @@ require 'zip'
 require 'fileutils'
 require 'syslog/logger'
 require 'pathname'
+require 'fuzzy_match'
 
 # I know using globals isn't great, but for these i don't care
 # Define a simple logger with concise format
 $logger = Logger.new(STDOUT)
-$logger.level = Logger::DEBUG
+$logger.level = Logger::INFO
 
 $logger.formatter = proc { |severity, datetime, progname, msg|
   "[#{severity}] #{msg}\n"
@@ -25,8 +26,13 @@ OptionParser.new do |opts|
   opts.on("-o", "--output-dir OUTPUT_DIR", "Specify a directory to output unzipped files to (default: './submissions')") do |out|
     $options[:out] = out 
   end
+  
   opts.on("-c", "--cs-identifier", "Create directories named after students csXXXXXX identifier instead of their name") do |cs|
     $options[:cs] = true
+  end
+
+  opts.on("-u", "--remove-unwanted", "Remove unwanted files (e.g. .vscode) in addition to warning about them. Unwanted extensions (.docx) will be kept") do |unwanted|
+    $options[:remove_unwanted] = true
   end
 end.parse!
 
@@ -50,6 +56,60 @@ def find_zip_current_dir()
   return file_to_extract
 end
 
+## File checks and sanitations
+def sanitize_filename(file)
+  sanitized = file.gsub(' ', '_') # Replace spaces with underscores
+  sanitized = sanitized.gsub(/_+/, '_') # only one underscore
+  # puts "Original: #{file}\nSanitized:#{sanitized}"
+  return sanitized 
+end
+
+# Should also maybe compare with 'default' main file if .c
+def file_empty?(file)
+  if File.zero?(file)
+    puts "\t\tEmpty? YES"
+    $logger.warn("\t\tEmpty? YES")
+  else
+    puts "\t\tEmpty? NO"
+  end
+end
+
+# check for unwated extensions or filenames
+def unwanted?(file)
+  filename = ['.vscode', '.idea', '.gitignore']
+  ext = ['.docx', '.exe', '.png']
+
+  if ext.include?(File.extname(file))
+    $logger.warn("\t\tUnwanted extension: #{File.extname(file)}")
+  end
+  if filename.include?(File.basename(file))
+    $logger.warn("\t\tUnwanted File: #{File.basename(file)}")
+    if $options[:remove_unwanted]
+      FileUtils.rm(file)
+    end
+  end
+end
+
+# in case the entry in the zip is another zip
+def extract_file(zip, entry, target)
+  zip.extract(entry, target)
+
+  if File.extname(entry.name) == '.zip'
+    Zip::File::open(target) do |sub_zip|
+      sub_zip.each do |sub_entry|
+        filename = sub_entry.name
+        filename = sanitize_filename(filename)
+        puts "\t   -#{filename}"
+        file_empty?(sub_entry.name)
+        unwanted?(sub_entry.name)
+        sub_target = File.join(File.dirname(target), filename)
+        sub_zip.extract(sub_entry, sub_target)
+      end
+    end
+    FileUtils.rm(target)
+  end
+end
+
 # Extract the zip file and restructure
 def extract_and_restructure_zip(file, target)
   # Match an uppercase letter followed by at least one lowercase letter, followed by 
@@ -57,25 +117,36 @@ def extract_and_restructure_zip(file, target)
   # matches e.g. '/Gurney_Halleck', as contained in zip downloaded from olat:
   # ita_Assignment_...'/Gurney_Halleck'_cs...
   pattern = nil
-  if $options[:cs]
+  if $options[:cs] # use cs-id instead of name for directory
     pattern = /cs[a-z]{2}[0-9]+/
   else
     pattern = /([A-Z][a-z]+)_([A-Z][a-z]+)/
   end
 
   Zip::File.open(file) do |zip|
+    student_name = nil
     zip.each do |entry|
       directory = String(entry.name.match(pattern))
+      if student_name != directory
+        student_name = directory
+        puts "\n"
+        $logger.info("#{student_name.gsub('_', ' ')}")
+      end
+
       filename = File.basename(Pathname.new(entry.name))
+      filename = sanitize_filename(filename)
+
+      puts "\t -#{filename}"
+      file_empty?(entry.name)
+      unwanted?(entry.name)
+
       target_path = File.join(target, directory)
       target_filepath = File.join(target_path, filename)
 
       $logger.debug("\n\tTarget: #{target}\n\tDirectory: #{directory}\n\tFilename: #{filename}\n\tTarget Path: #{target_path}\n\tFull: #{target_filepath}")
 
-      # Maybe I misread the docs for this method, but giving the 'target_path' results in the 
-      # directory for a student not being created
       FileUtils.mkdir_p(File.dirname(target_filepath)) unless Dir.exist?(target_filepath)
-      zip.extract(entry, target_filepath)
+      extract_file(zip, entry, target_filepath)
     end
   end
 end
